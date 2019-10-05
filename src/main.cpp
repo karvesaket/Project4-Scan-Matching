0f4a122403ec841a16b948773fa197e997f91771
SAKET KARVE <saketk@park.key.seas.upenn.edu>
2019-10-04 21:10:46 -0400

    Initial Commit - took code from Character recognition

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file      main.cpp
+ * @brief     Scan Matching
+ * @authors   Saket Karve
+ * @date      2019
+ * @copyright University of Pennsylvania
+ */
+
+#include <cstdio>
+#include <scan_matching/mlp.h>
+#include <scan_matching/common.h>
+#include "testing_helpers.hpp"
+
+
+
+int main(int argc, char* argv[]) {
+	std::cout << "MAIN\n";
+}
